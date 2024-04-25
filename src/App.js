b5ded49a5d74933d4b7b06e05716813d8ff7a12a
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2024-04-25 10:28:24 +0530

    "Start"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="">
-      
+      <Routes>
+        <Route />
+      </Routes>
     </div>
   );
 }
