fa58d162d00ce9c426a7efac23361cdaa30a935f
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2024-04-26 02:44:12 +0530

    "added some"

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button} from "@nextui-org/react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button, Dropdown, DropdownTrigger, Avatar, DropdownMenu, DropdownItem} from "@nextui-org/react";
 import logo from '../assets/logo.png';
+import profile from '../assets/profile.jpg'
 
 export default function NavBar() {
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
   return (
     <div>
-         <Navbar onMenuOpenChange={setIsMenuOpen} shouldHideOnScroll={true} isBlurred={true} className='navbar' >
+         <Navbar onMenuOpenChange={setIsMenuOpen} shouldHideOnScroll isBlurred={true} className='navbar'>
       <NavbarContent>
         <NavbarMenuToggle
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
@@ -21,37 +22,68 @@ export default function NavBar() {
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
           <Link color="foreground" href="#">
-            Home &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|
+            Home &nbsp;&nbsp;&nbsp;|
           </Link>
         </NavbarItem>
         
         <NavbarItem>
           <Link color="foreground" href="#">
-          &nbsp;&nbsp;&nbsp;Shop &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|
+          &nbsp;Shop &nbsp;&nbsp;&nbsp;|
           </Link>
         </NavbarItem>
         <NavbarItem>
           <Link color="foreground" href="#">
-          &nbsp;&nbsp;&nbsp;Elements &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|
+          &nbsp;Elements &nbsp;&nbsp;&nbsp;|
           </Link>
         </NavbarItem>
         <NavbarItem>
           <Link color="foreground" href="#">
-          &nbsp;&nbsp;&nbsp;Contact
+          &nbsp;Contact
           </Link>
         </NavbarItem>
                 
       </NavbarContent>
       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
+
+      <NavbarItem>
+        <i style={{fontSize:'22px'}} class="bi bi-bag"></i>2
         </NavbarItem>
+
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#" color='warning'>Login</Link>
+        </NavbarItem>
+
         <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
+          <Button as={Link} color="warning" href="#" variant="flat">
             Sign Up
           </Button>
         </NavbarItem>
+        
       </NavbarContent>
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              color="warning"
+              name=""
+              size="sm"
+              src={profile}
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem key="profile" className="h-14 gap-2">
+              <p className="font-semibold">Signed in as</p>
+              <p className="font-semibold">demo@gmail.com</p>
+            </DropdownItem>
+            <DropdownItem key="my_profile" color='warning'>My Profile</DropdownItem>
+            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+            <DropdownItem key="logout" color="danger">
+              Log Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       <NavbarMenu>
         
           <NavbarMenuItem>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,28 @@ module.exports = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        black: "#000000",
+        blue: {
+          50: "#e6f1fe",
+          100: "#cce3fd",
+          200: "#99c7fb",
+          300: "#66aaf9",
+          400: "#338ef7",
+          500: "#006FEE",
+          600: "#005bc4",
+          700: "#004493",
+          800: "#002e62",
+          900: "#001731",
+        },
+        // .. rest of the colors
+      },
+    },
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [nextui({
+    addCommonColors: true
+  })],
 };
