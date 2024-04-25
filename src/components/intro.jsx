a6733cf935dd68aa74fb39c11e95a4977f01514c
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2024-04-25 17:28:24 +0530

    "some changes"

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import img1 from '../assets/introimg1.png';
 import img2 from '../assets/introimg2.png';
 import './Intro.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 export default function Intro() {
   return (
@@ -11,13 +12,17 @@ export default function Intro() {
             <img src={img1} alt="" width={980}/>
         </div>
         <div className='intro_content'>
-            <div style={{color:"orange"}}>New Collection</div><br />
-            <div style={{fontSize:"40px",fontWeight:"bolder"}}>Purposeful Goods and Gifts Crafted by Hand</div>
+            <div style={{color:"orange"}}>New Collection</div>
+            <div style={{fontSize:"40px",fontWeight:"bold"}} className='font'>Purposeful Goods and Gifts Crafted by Hand</div>
             <div style={{fontSize:"20px"}}>Supporting local makers since 2024</div><br />
-            <div><button>SHOP NOW | </button></div>
+            <div>
+              <button className='shop_btn'>
+                SHOP NOW &nbsp;&nbsp;<i style={{borderLeft:"1px solid black",padding:"13px 0px 13px 10px"}} class="bi bi-arrow-right"></i>
+              </button>
+            </div>
         </div>
         <div>
-            <img src={img2} alt="" width={1000}/>
+            <img src={img2} alt="" width={1050}/>
         </div>
     </div>
     </center>
