f712a97c78e32a38f1ac39ec5b49ec5d5714d1b4
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2024-04-28 00:03:29 +0530

    "changes"

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -3,8 +3,10 @@ import img1 from '../assets/introimg1.png';
 import img2 from '../assets/introimg2.png';
 import './Intro.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function Intro() {
+  const naviagte = useNavigate();
   return (
     <center>
     <div className='intro_flex'>
@@ -16,7 +18,7 @@ export default function Intro() {
             <div style={{fontSize:"40px",fontWeight:"bold"}} className='font'>Purposeful Goods and Gifts Crafted by Hand</div>
             <div style={{fontSize:"20px"}}>Supporting local makers since 2024</div><br />
             <div>
-              <button className='shop_btn'>
+              <button className='shop_btn' onClick={()=>naviagte("/shoppage")}>
                 SHOP NOW &nbsp;&nbsp;<i style={{borderLeft:"1px solid black",padding:"13px 0px 13px 10px"}} class="bi bi-arrow-right"></i>
               </button>
             </div>
