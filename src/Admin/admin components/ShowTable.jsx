0e682e38864983c9bccb77a47b24f6a3c31831f7
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2024-05-29 01:20:42 +0530

    "changes"

--- src/Admin/admin components/ShowTable.jsx
+++ src/Admin/admin components/ShowTable.jsx
@@ -1,43 +1,51 @@
 import React, { useEffect, useState } from "react";
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell} from "@nextui-org/react";
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Tooltip} from "@nextui-org/react";
 import axios from "axios";
+import {EditIcon} from "./EditIcon";
+import {DeleteIcon} from "./DeleteIcon";
 
 export default function ShowTable() {
   const [products,setProducts]=useState([]);
 
   useEffect(()=>{
     axios.get("http://localhost:3001/getProduct")
-    .then((e)=>setProducts(e.data))
+    .then((res)=>setProducts(res.data))
     .catch((err)=>console.log(err));
-  })
+  },[]);
   return (
     <Table aria-label="Example static collection table">
       <TableHeader>
         <TableColumn>NAME</TableColumn>
         <TableColumn>ROLE</TableColumn>
         <TableColumn>STATUS</TableColumn>
+        <TableColumn>ACTIONS</TableColumn>
       </TableHeader>
       <TableBody>
-        <TableRow key="1">
-          <TableCell>Tony Reichert</TableCell>
-          <TableCell>CEO</TableCell>
-          <TableCell>Active</TableCell>
-        </TableRow>
-        <TableRow key="2">
-          <TableCell>Zoey Lang</TableCell>
-          <TableCell>Technical Lead</TableCell>
-          <TableCell>Paused</TableCell>
-        </TableRow>
-        <TableRow key="3">
-          <TableCell>Jane Fisher</TableCell>
-          <TableCell>Senior Developer</TableCell>
-          <TableCell>Active</TableCell>
-        </TableRow>
-        <TableRow key="4">
-          <TableCell>William Howard</TableCell>
-          <TableCell>Community Manager</TableCell>
-          <TableCell>Vacation</TableCell>
-        </TableRow>
+      {
+        products.map((e)=>{
+          return(
+          <TableRow>
+            <TableCell>{e.proname}</TableCell>
+            <TableCell>{e.prorate}</TableCell>
+            <TableCell>{e.category}</TableCell>
+            <TableCell>
+              <div className="relative flex items-center gap-2">
+                <Tooltip content="Edit user">
+                  <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                    <EditIcon />
+                  </span>
+                </Tooltip>
+                <Tooltip color="danger" content="Delete user">
+                  <span className="text-lg text-danger cursor-pointer active:opacity-50">
+                    <DeleteIcon />
+                  </span>
+                </Tooltip>
+              </div>
+            </TableCell>
+          </TableRow>
+          )
+        })
+      }
       </TableBody>
     </Table>
   );
