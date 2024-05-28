c9009104833a0c78e564d877c253d5f3866f0c51
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2024-05-28 23:09:10 +0530

    "changes"

--- src/Admin/AdminHome.jsx
+++ src/Admin/AdminHome.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import "./AdminHome.css"
 import AdminNav from './AdminNav';
+import img from "../assets/rpro.png"
 
 export default function AdminHome() {
   const [proname,setProname] = useState();
@@ -8,9 +9,10 @@ export default function AdminHome() {
   return (
     <center className='admin_home_flex'>
       <AdminNav/>
-      <div className='p_head_flex' style={{width:"85%",marginTop:"15px",marginBottom:"15px"}}>
-                <div className='font' style={{fontSize:"33px"}}>Admin Portal</div>
-                <hr style={{border:"1.5px solid #",minWidth:"420px"}}/>
+      <hr />
+      <div className='p_head_flex' style={{width:"85%",marginTop:"25px",marginBottom:"15px"}}>
+                <div className='font' style={{fontSize:"33px"}}>Product Inventory</div>
+                <hr style={{border:"1.5px solid #",minWidth:"540px"}}/>
                 <div className='p_head_arrows' style={{gap:"10px"}}>
                     <div><button className='filter_btn'>
                         Garden</button></div>
@@ -21,9 +23,9 @@ export default function AdminHome() {
                     <div><button className='filter_btn'>
                         Homewares</button></div>
                 </div>
-            </div>
-            <hr />
-        <div className='admin_main_flex' style={{marginTop:"20px"}}>
+        </div>
+        
+        <div className='admin_main_flex'>
           <div>
               <form action="#" className='admin_add_pro'>
                   <input className='shop_btn' type="text" placeholder='Product Name' />
@@ -37,9 +39,26 @@ export default function AdminHome() {
               </form>
           </div>
           <div>
-              <img src="https://www.firstpalette.com/images/craft-mainpic-large/accordionpaperflowers-main.jpg" alt="" width={500} />
+              <img src={img} alt="" width={370} />
           </div>
-        </div>    
+        </div>
+        <hr style={{marginTop:"25px"}}/>
+
+        <div className='p_head_flex' style={{width:"85%",marginTop:"25px",marginBottom:"15px"}}>
+                <div className='font' style={{fontSize:"33px"}}>Product Inventory</div>
+                <hr style={{border:"1.5px solid #",minWidth:"540px"}}/>
+                <div className='p_head_arrows' style={{gap:"10px"}}>
+                    <div><button className='filter_btn'>
+                        Garden</button></div>
+                    <div><button className='filter_btn'>
+                        Kitchen</button></div>
+                    <div><button className='filter_btn' >
+                        Candles</button></div>
+                    <div><button className='filter_btn'>
+                        Homewares</button></div>
+                </div>
+        </div>
+
     </center>
   )
 }
--- src/Admin/AdminNav.jsx
+++ src/Admin/AdminNav.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button } from "@nextui-org/react";
 import { useNavigate } from 'react-router-dom';
 import logo from '../assets/logo.png'
-import profile from '../assets/profile.jpg';
 
 export default function AdminNav() {
 
