8cd4faf5a39c0978782195914a68c441b69922c7
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2024-06-11 10:25:24 +0530

    "register changes"

--- src/Authenticate/SignUp.jsx
+++ src/Authenticate/SignUp.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignUp.css';
 import img from '../assets/signup.jpeg';
 import logo from "../assets/logo.png";
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
+
+  const navigate = useNavigate();
+
+  const [username, setUsername]=useState();
+  const [email, setEmail]=useState();
+  const [password, setPassword]=useState();
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    axios.post("http://localhost:3001/register", {username, email, password})
+    .then((e)=>{
+      navigate("/login");
+    }).catch(err=>console.log(err))
+  }
   return (
     <center>
         <div className="login_flex">
@@ -13,14 +29,22 @@ export default function SignUp() {
                       <div className='font' style={{fontSize:"27px",color:"gray"}}>Create account</div>
                   </div>
                   <div style={{color:"black",fontSize:"14px"}}>Please enter your details</div>
-                  <div><input type="text" placeholder='Enter your name' className='shop_btn' style={{width:"300px"}}/></div>
-                  <div><input type="text" placeholder='Enter your email' className='shop_btn' style={{width:"300px"}}/></div>
-                  <div><input type="text" name="" id="" placeholder='Enter your password' className='shop_btn' style={{width:"300px"}}/></div>
-                  <div>
-                      <button className='shop_btn'>
-                          Sign Up &nbsp;&nbsp;<i style={{borderLeft:"1px solid black",padding:"13px 0px 13px 10px"}} class="bi bi-arrow-right"></i>
-                      </button>
-                  </div>
+                  <form action='#' onSubmit={handleSubmit} style={{display:"flex",flexDirection:"column",gap:"15px",alignItems:"flex-start"}}>
+                    <div><input type="text" placeholder='Enter your name' className='shop_btn' style={{width:"300px"}} required
+                      onChange={(e)=>setUsername(e.target.value)}
+                    /></div>
+                    <div><input type="email" placeholder='Enter your email' className='shop_btn' style={{width:"300px"}} required
+                      onChange={(e)=>setEmail(e.target.value)}
+                    /></div>
+                    <div><input type="text" name="" id="" placeholder='Enter your password' className='shop_btn' style={{width:"300px"}}
+                      onChange={(e)=>setPassword(e.target.value)}
+                    /></div>
+                    <div>
+                        <button className='shop_btn' type='submit'>
+                            Sign Up &nbsp;&nbsp;<i style={{borderLeft:"1px solid black",padding:"13px 0px 13px 10px"}} class="bi bi-arrow-right"></i>
+                        </button>
+                    </div>
+                  </form>
                   <a href="/login" style={{color:"orange"}}>Already have an account?</a>
                   <div style={{display:'flex',alignItems:"center",fontSize:'14px'}}>
                       <hr style={{border:"1px solid #00000012",minWidth:"45px"}}/> &nbsp;
