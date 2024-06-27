20a25342fee2902ffb8379d92365302a42f0b96d
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2024-06-27 14:14:20 +0530

    "changes"

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react'
 import './ShopPage.css'
 import RCard from '../cards/RCard'
 import NavBar from '../components/NavBar'
-import Footer from '../components/Footer.jsx'
-import axios from 'axios'
+import Footer from '../components/Footer.jsx';
+import axios from 'axios';
 
 export default function ShopPage() {
     const [products,setProducts]=useState([]);
