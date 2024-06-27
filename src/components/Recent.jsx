356ff140cc3f2fef66e1c9418049399b1fca9923
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2024-06-27 14:02:05 +0530

    "changes"

--- src/components/Recent.jsx
+++ src/components/Recent.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Recent.css";
 import RCard from '../cards/RCard';
+import axios from 'axios';
 
 export default function Recent() {
+
+    const [products,setProducts]=useState([]);
+
+    useEffect(()=>{
+        axios.get('http://localhost:3001/getProduct')
+        .then(e =>{
+        setProducts(e.data);
+        })
+        .catch(err=>console.log(err));
+        }, []);
+
+    const [filter, setFilter] = useState('recommended');
+
+    const filteredProducts = products.filter((item)=> {
+
+        const categoryMatch = filter === 'recommended' || item.category2 === filter;
+    
+        return categoryMatch;
+      });
+
   return (
     <center>
         <div className='recent_flex'>
@@ -12,23 +33,18 @@ export default function Recent() {
                 <div className='p_head_arrows' style={{gap:"10px"}}>
                     <div><button className='filter_btn'>
                         Recent View</button></div>
-                    <div><button className='filter_btn'>
+                    <div><button className='filter_btn' onClick={()=>setFilter("best seller")}>
                         Bestseller</button></div>
-                    <div><button className='filter_btn'>
+                    <div><button className='filter_btn' onClick={()=>setFilter("popular")}>
                         Popular</button></div>
-                    <div><button className='filter_btn'>
+                    <div><button className='filter_btn' onClick={()=>setFilter("recommended")}>
                         Recommended</button></div>
                 </div>
             </div>
             <div className='r_pro_flex'>
-                <RCard/>
-                <RCard/>
-                <RCard/>
-                <RCard/>
-                <RCard/>
-                <RCard/>
-                <RCard/>
-                <RCard/>
+                {filteredProducts?.map(e=>(
+                    <RCard imgurl={e.imgurl} proname={e.proname} prorate={e.prorate} _id={e._id}/>
+                ))}
             </div>
         </div>
     </center>
