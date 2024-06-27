b072569595d7be182b0456eff9cca03a661f9e80
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2024-06-27 13:36:17 +0530

    "changes"

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,14 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './ShopPage.css';
 import RCard from '../cards/RCard';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer.jsx';
+import axios from 'axios';
 
 export default function ShopPage() {
+    const [products,setProducts]=useState([]);
 
     useEffect(()=>{
         window.scrollTo(0,0);
-    })
+        axios.get('http://localhost:3001/getProduct')
+        .then(e =>{
+        setProducts(e.data);
+        })
+        .catch(err=>console.log(err));
+        }, []);
+
+    const [filter, setFilter] = useState('all');
+    const [search, setSearch] = useState('');
+    const handleFilterChange = (selectedFilter) => {
+        setFilter(selectedFilter);
+    };
+    const handleSearchChange = (event) => {
+        setSearch(event.target.value);
+    }
+
+    const filteredProducts = products.filter( (item)=> {
+
+        const categoryMatch = filter === 'all' || item.category === filter;
+    
+        const searchMatch = item.proname.toLowerCase().includes(search.toLowerCase());
+    
+        return categoryMatch&searchMatch;
+      });
 
   return (
     <center>
@@ -33,14 +58,9 @@ export default function ShopPage() {
                 </div>
             </div>
             <div className='r_pro_flex' style={{marginBottom:"50px"}}>
-                <RCard/>
-                <RCard/>
-                <RCard/>
-                <RCard/>
-                <RCard/>
-                <RCard/>
-                <RCard/>
-                <RCard/>
+            {filteredProducts?.map(e=>(
+                <RCard imgurl={e.imgurl} proname={e.proname} prorate={e.prorate} _id={e._id}/>
+                ))}
             </div>
         </div>
         <hr style={{width:"85%",border:"1px solid #00000012",marginBottom:"60px"}}/>
