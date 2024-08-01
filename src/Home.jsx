6e12c56392e3bd2387932f51465a20f61e3e270f
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2024-08-01 23:22:36 +0530

    "small changes"

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import NavBar from './components/NavBar';
 import Intro from './components/intro';
 import Feature from './components/Feature';
